a85a7cb942cb892ca4b368416a73c4b605a9f900
wl4dig3 <wladige@gmail.com>
2024-01-27 19:40:17 -0300

    feat: 4_colores

---
+++ 4_colores.js
@@ -0,0 +1,23 @@
+function pintar(color) {
+    let ele1 = document.getElementById('ele1');
+    ele1.addEventListener("click", pintar);
+    ele1.style.backgroundColor = color;
+}
+function pintar2(color) {
+    let ele2 = document.getElementById('ele2');
+   ele2.addEventListener("click", pintar);
+    ele2.style.backgroundColor = color;
+}
+function pintar3(color) {
+    let ele3 = document.getElementById('ele3');
+   ele3.addEventListener("click", pintar);
+    ele3.style.backgroundColor = color;
+}
+function pintar4(color) {
+    let ele4 = document.getElementById('ele4');
+   ele4.addEventListener("click", pintar);
+    ele4.style.backgroundColor = color;
+}
+
+    
+    
